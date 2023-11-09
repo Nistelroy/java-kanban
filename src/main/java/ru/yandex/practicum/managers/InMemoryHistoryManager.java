5bdd9afbe07838e019100f46ef1e0743cb79bcd4
Nistel <duz3000@gmail.com>
2023-11-09 11:08:05 +0300

    refactor : changed my mind because there will be outdated data

--- src/main/java/ru/yandex/practicum/managers/InMemoryHistoryManager.java
+++ src/main/java/ru/yandex/practicum/managers/InMemoryHistoryManager.java
@@ -1,7 +1,5 @@
 package main.java.ru.yandex.practicum.managers;
 
-import main.java.ru.yandex.practicum.tasks.Epic;
-import main.java.ru.yandex.practicum.tasks.Subtask;
 import main.java.ru.yandex.practicum.tasks.Task;
 
 import java.util.LinkedList;
@@ -17,18 +15,7 @@ public class InMemoryHistoryManager implements HistoryManager {
             historyList.remove(0);
         }
         if (!(task == null)) {
-            if (task.getClass().equals(Task.class)) {
-                Task taskCopy = new Task(task.getName(), task.getDescription(), task.getId(), task.getStatus());
-                historyList.add(taskCopy);
-            } else if (task.getClass().equals(Epic.class)) {
-                Epic taskCopy = new Epic(task.getName(), task.getDescription(), task.getId(), task.getStatus(),
-                        ((Epic) task).getIdSubtask());
-                historyList.add(taskCopy);
-            } else if (task.getClass().equals(Subtask.class)) {
-                Subtask taskCopy = new Subtask(task.getName(), task.getDescription(), task.getId(),
-                        task.getStatus(), ((Subtask) task).getIdEpic());
-                historyList.add(taskCopy);
-            }
+            historyList.add(task);
         }
     }
 
