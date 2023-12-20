7fd0301a476d545888848e416a530fe1ca7bd4a6
Nistel <duz3000@gmail.com>
2023-12-20 20:09:18 +0300

    style : optim imp

--- src/main/java/ru/yandex/practicum/managers/file/FileBackedTasksManager.java
+++ src/main/java/ru/yandex/practicum/managers/file/FileBackedTasksManager.java
@@ -15,7 +15,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static main.java.ru.yandex.practicum.managers.file.TaskConverter.epicFromString;
